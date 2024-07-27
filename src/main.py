9e5da7e959b735e0685d3de93226ed77e3d413ff
UMCSResearchLog <victor.wen@umconnect.umt.edu>
2024-07-26 18:28:39 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,35 +3,34 @@ import re
 from xml.etree import ElementTree as ET
 
 def check_log4j_vulnerabilities_in_file(content):
-    # CVE scores (example values)
+    # CVE scores
     cve_scores = {
         "CVE-2021-44228": 10.0,
         "CVE-2021-45046": 9.0,
         "CVE-2021-45105": 7.5,
+        "CVE-2021-44832": 6.6,
         "CVE-2019-17571": 9.8,
         "CVE-2020-9488": 7.5,
         "CVE-2021-4104": 7.5,
-        "CVE-2017-5645": 6.8,
-        "Potential misconfiguration": 5.0  # Arbitrary lower score for potential misconfigurations
+        "CVE-2022-23302": 9.0,
+        "CVE-2022-23305": 9.1,
+        "CVE-2022-23307": 10.0,
+        "Potential misconfiguration": 5.0
     }
 
     # Patterns to search for
     vulnerabilities = {
         "Vulnerable Log4j v2 version": {
-            "pattern": re.compile(r'log4j\-(2\.14\.[0-9]|[2-9]\.[0-9]+|2\.[1-9]\.[0-9]+|2\.[0-9]\.[1-9]+)\.'),
-            "CVE": ["CVE-2021-44228", "CVE-2021-45046", "CVE-2021-45105"]
+            "pattern": re.compile(r'log4j\-(2\.(0|1[0-6]|17\.[01])(\.\d+)?(?!\.2)(?!\.4))\.'),
+            "CVE": ["CVE-2021-44228", "CVE-2021-45046", "CVE-2021-45105", "CVE-2021-44832"]
         },
         "Log4j v1 found, which is vulnerable": {
             "pattern": re.compile(r'log4j\-1\.[0-9]+\.[0-9]+'),
+            "CVE": ["CVE-2019-17571", "CVE-2020-9488", "CVE-2021-4104", "CVE-2022-23302", "CVE-2022-23305", "CVE-2022-23307"]
+        },
+        "SocketServer found, possible vulnerability": {
+            "pattern": re.compile(r'org\.apache\.log4j\.net\.SocketServer'),
             "CVE": ["CVE-2019-17571"]
-        },
-        "JndiLookup.class found, possible vulnerability": {
-            "pattern": re.compile(r'JndiLookup\.class'),
-            "CVE": ["CVE-2021-44228"]
-        },
-        "JNDI lookup pattern found, possible vulnerability": {
-            "pattern": re.compile(r'\$\{jndi:'),
-            "CVE": ["CVE-2021-44228"]
         },
         "SMTPAppender found, possible vulnerability": {
             "pattern": re.compile(r'org\.apache\.log4j\.net\.SMTPAppender'),
@@ -41,13 +40,17 @@ def check_log4j_vulnerabilities_in_file(content):
             "pattern": re.compile(r'org\.apache\.log4j\.net\.JMSAppender'),
             "CVE": ["CVE-2021-4104"]
         },
-        "SocketServer found, possible vulnerability": {
-            "pattern": re.compile(r'org\.apache\.log4j\.net\.SocketServer'),
-            "CVE": ["CVE-2019-17571"]
+        "JMSSink found, possible vulnerability": {
+            "pattern": re.compile(r'org\.apache\.log4j\.net\.JMSSink'),
+            "CVE": ["CVE-2022-23302"]
         },
-        "Chainsaw UDPReceiver found, possible vulnerability": {
-            "pattern": re.compile(r'org\.apache\.log4j\.chainsaw\.UDPReceiver'),
-            "CVE": ["CVE-2017-5645"]
+        "JDBCAppender found, possible vulnerability": {
+            "pattern": re.compile(r'org\.apache\.log4j\.jdbc\.JDBCAppender'),
+            "CVE": ["CVE-2022-23305"]
+        },
+        "Chainsaw component found, possible vulnerability": {
+            "pattern": re.compile(r'org\.apache\.log4j\.chainsaw'),
+            "CVE": ["CVE-2022-23307"]
         },
         "Suspicious configuration pattern found: logger.addAppender": {
             "pattern": re.compile(r'logger\.addAppender'),
@@ -63,22 +66,33 @@ def check_log4j_vulnerabilities_in_file(content):
 
     # Check for all vulnerabilities
     for description, details in vulnerabilities.items():
-        if details["pattern"].search(content):
+        match = details["pattern"].search(content)
+        if match:
+            version = match.group(0)
             for cve in details["CVE"]:
-                found_vulnerabilities.append((description, cve, cve_scores.get(cve, 0)))
+                found_vulnerabilities.append((description, version, cve, cve_scores.get(cve, 0)))
 
     return found_vulnerabilities
 
 def check_pom_file_for_vulnerable_versions(file_path):
     vulnerable_versions = {
-        "log4j-core": ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "2.10", "2.11", "2.12", "2.13", "2.14", "2.14.1", "2.15", "2.16"],
-        "log4j-api": ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "2.10", "2.11", "2.12", "2.13", "2.14", "2.14.1", "2.15", "2.16"],
-        "log4j": ["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10", "1.11", "1.12", "1.13", "1.14", "1.15", "1.16", "1.17", "1.18", "1.19"]
+        "log4j-core": ["2.0.0", "2.1.0", "2.2.0", "2.3.0", "2.4.0", "2.5.0", "2.6.0", "2.7.0", "2.8.0", "2.9.0", "2.10.0", "2.11", "2.12", "2.13", "2.14", "2.14.1", "2.15.0", "2.16", "2.17.0", "2.17.1"],
+        "log4j-api": ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "2.10", "2.11", "2.12", "2.13", "2.14", "2.14.1", "2.15", "2.16", "2.17.0", "2.17.1"],
+       "log4j": [
+            "1.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4", 
+            "1.1", "1.1b1", "1.1b2", "1.1b3", "1.1b4", 
+            "1.1b5", "1.1b6", "1.1b7", "1.1.1", "1.1.2", 
+            "1.1.3", "1.2", "1.2.1", "1.2.2", "1.2.3", 
+            "1.2.4", "1.2.5", "1.2.6", "1.2.7", "1.2.8", 
+            "1.2.9", "1.2.10", "1.2.11", "1.2.12", "1.2.13", 
+            "1.2.14", "1.2.15", "1.2.16", "1.2.17"
+        ]
     }
     cve_scores = {
         "CVE-2021-44228": 10.0,
         "CVE-2021-45046": 9.0,
         "CVE-2021-45105": 7.5,
+        "CVE-2021-44832": 6.6,
         "CVE-2019-17571": 9.8
     }
 
@@ -101,12 +115,12 @@ def check_pom_file_for_vulnerable_versions(file_path):
 
                 if artifact_id_text in vulnerable_versions and version_text in vulnerable_versions[artifact_id_text]:
                     cves = {
-                        "log4j-core": ["CVE-2021-44228", "CVE-2021-45046", "CVE-2021-45105"],
-                        "log4j-api": ["CVE-2021-44228", "CVE-2021-45046", "CVE-2021-45105"],
-                        "log4j": ["CVE-2019-17571"]
+                        "log4j-core": ["CVE-2021-44228", "CVE-2021-45046", "CVE-2021-45105", "CVE-2021-44832"],
+                        "log4j-api": ["CVE-2021-44228", "CVE-2021-45046", "CVE-2021-45105", "CVE-2021-44832"],
+                        "log4j": ["CVE-2019-17571", "CVE-2020-9488", "CVE-2021-4104", "CVE-2022-23302", "CVE-2022-23305", "CVE-2022-23307"]
                     }
                     for cve in cves[artifact_id_text]:
-                        found_vulnerabilities.append((f"Vulnerable {artifact_id_text} version: {version_text}", cve, cve_scores.get(cve, 0)))
+                        found_vulnerabilities.append((f"Vulnerable {artifact_id_text} version: {version_text}", version_text, cve, cve_scores.get(cve, 0)))
 
     except ET.ParseError as e:
         print(f"Could not parse {file_path}: {e}")
@@ -137,7 +151,7 @@ def check_log4j_vulnerabilities_in_directory(directory_path):
         print("No vulnerabilities found.")
         return
 
-   # Sort by CVE score
+    # Sort by CVE score
     sorted_vulnerabilities = sorted(
         all_vulnerabilities,
         key=lambda x: max(v[2] for v in x[1]),
@@ -148,8 +162,15 @@ def check_log4j_vulnerabilities_in_directory(directory_path):
     print("Vulnerabilities found:")
     for file_path, vulnerabilities in sorted_vulnerabilities:
         print(f"File: {file_path}")
-        for description, cve, score in vulnerabilities:
-            print(f"  - {description}: {cve} (Score: {score})")
+        for description, version, cve, score in vulnerabilities:
+            recommendation = get_patch_recommendation(version, cve)
+            print(f"  - {description} ({version}): {cve} (Score: {score})")
+            print(f"    Recommendation: {recommendation}")
+
+def get_patch_recommendation(version, cve):
+    if "log4j-1" in version:
+        return "Upgrade to Log4j 2.x or remove the component if not needed."
+    return "Upgrade to Log4j 2.17.2 or later or disable unneeded components."
 
 if __name__ == "__main__":
     import argparse
